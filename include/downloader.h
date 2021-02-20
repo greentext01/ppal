bcc4c726972f6d90e917e498f335cbfce48e6252
greentext01 <66892647+greentext01@users.noreply.github.com>
2021-02-20 05:02:33 -0500

    Made the build work

--- include/downloader.h
+++ include/downloader.h
@@ -1,15 +1,21 @@
+#ifndef DOWNLOADER_H
+#define DOWNLOADER_H
+
 #include <curl/curl.h>
 #include <error.h>
+#include <stdio.h>
 
 #include <iostream>
 
 class Downloader {
    private:
-    CURL *curlHandle;
+    CURL* curlHandle;
     CURLcode curlResponse;
+    FILE* outputFileDescriptor;
 
    public:
     Downloader(std::string url, std::string outputPath);
     void startDownload(bool showProgressBar);
-    ~Downloader();
 };
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
-int main() {}
+#include <ppal.h>
+
+int main() {
+    Downloader dwn("https://www.angio.net/pi/digits/pi1000000.txt",
+                   "pi1million.txt");
+    dwn.startDownload(false);
+    return 0;
+}
