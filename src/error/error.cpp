3c167a61478543c898a08c1864ad6a05b564d7fd
Olivier Audet-Yang <oaudetyang@gmail.com>
2021-02-22 18:59:00 -0500

    Added comments and cleaned up code

--- src/error/error.cpp
+++ src/error/error.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
-void error(std::string message, int exitStatus = 0) {
+/**
+ * @brief Print message to stderr
+ *
+ * @param message The message to print
+ * @param exitStatus The exit status (defaults to 1)
+ */
+void error(std::string message, int exitStatus = 1) {
     std::cerr << message;
     exit(exitStatus);
 }
 
+/**
+ * @brief Throw error if condtion is met
+ *
+ * @param condition The condition that needs to be met
+ * @param errorMessage The message to be printed out to stderr
+ */
 void throwErrorIf(bool condition, std::string errorMessage) {
     if (condition) {
         error(errorMessage);
